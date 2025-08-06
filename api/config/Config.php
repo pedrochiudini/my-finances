d424c16b3125fb4ea42c0c4578ee3827cde7f31f
pedrochiudini <pedrochiudinil@gmail.com>
2025-08-06 20:37:09 -0300

    chore: configs do database render

--- api/config/Config.php
+++ api/config/Config.php
@@ -10,7 +10,7 @@ define('DB_USERNAME', 'postgres');
 define('DB_PASSWORD', 'postgres');
 */
 
-define('DB_HOST', 'dpg-d29tr2ur433s739sb2f0-a');
+define('DB_HOST', 'dpg-d29tr2ur433s739sb2f0-a.oregon-postgres.render.com');
 define('DB_PORT', '5432');
 define('DB_NAME', 'myfinances_dbrender');
 define('DB_USERNAME', 'myfinances');
